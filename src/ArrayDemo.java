c27d6f345fdfa91bfa11277a55473ce4ac337f2b
anukul mehta <mehtaanukul@gmail.com>
2018-02-17 17:05:27 +0530

    java array basic example

---
+++ src/ArrayDemo.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Anukul-PC on 17-02-2018.
+ */
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int a[] = new int[10];
+
+        a[0] = 20;
+
+        System.out.println(a);
+
+    }
+}
