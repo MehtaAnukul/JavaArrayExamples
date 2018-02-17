6195d749f2754ddceb6c32270f6503ce30f83723
anukul mehta <mehtaanukul@gmail.com>
2018-02-17 17:49:22 +0530

    java array length

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,13 +1,23 @@
 /**
  * Created by Anukul-PC on 17-02-2018.
  */
+import java.lang.String;
 public class ArrayDemo {
     public static void main(String[] args) {
-        int a[] = new int[10];
 
-        a[0] = 20;
+        String a[] = {"Mimoh", "Anukul", "Kodee", "Sagar", "Naresh", "Vishal", "Rahul"};
 
-        System.out.println(a);
+        for (int i = 0; i < a.length; i++) {
+            {
+                String sub = a[i];
 
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(sub.charAt(j));
+                    //System.out.println(" ");
+                }
+                System.out.println();
+                // System.out.println(a.length);
+            }
+        }
     }
 }
